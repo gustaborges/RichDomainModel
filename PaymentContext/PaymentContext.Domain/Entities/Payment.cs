aa2fc98131f7b09dcda54fee94e30cd6ad9baffd
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-04-23 11:11:21 -0300

    BoletoPayment Entity

--- PaymentContext/PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Payment.cs
@@ -4,7 +4,7 @@ namespace PaymentContext.Domain.Entities
 {
     public abstract class Payment
     {
-        public string Number { get; set; }
+        public string Number { get; set; } // 8-9 dígitos
         public DateTime PaidDate { get; set; }
         public DateTime ExpiredDate { get; set; }
         public decimal Total { get; set; }
@@ -12,11 +12,14 @@ namespace PaymentContext.Domain.Entities
         public string Payer { get; set; } // Proprietário do pagamento
         public string Document { get; set; }
         public string Address { get; set; }
+        public string Email { get; set; }
+
     }
 
     public class BoletoPayment : Payment
     {
-
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; } // 12 dígitos
     }
 
     public class CreditCardPayment : Payment
