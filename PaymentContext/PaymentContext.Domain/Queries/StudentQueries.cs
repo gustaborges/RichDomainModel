69cb985a27f754c0b50e53228c7840712f6677d7
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-04-28 12:44:33 -0300

    Created Query Sample

---
+++ PaymentContext/PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq.Expressions;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Queries
+{
+    public static class StudentQueries
+    {
+        // Pode ser usado, por exemplo, no comando Where do Linq :)
+        public static Expression<Func<Student, bool>> GetStudent(string document)
+        {
+            return x => x.Document.Number == document;
+        }
+    }
+}
