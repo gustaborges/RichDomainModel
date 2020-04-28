731647866b6ebb1137fd711ae94bcb7f555cb395
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-04-28 12:58:50 -0300

    Created tests for StudentQueries

--- PaymentContext/PaymentContext.Domain/Queries/StudentQueries.cs
+++ PaymentContext/PaymentContext.Domain/Queries/StudentQueries.cs
@@ -7,7 +7,7 @@ namespace PaymentContext.Domain.Queries
     public static class StudentQueries
     {
         // Pode ser usado, por exemplo, no comando Where do Linq :)
-        public static Expression<Func<Student, bool>> GetStudent(string document)
+        public static Expression<Func<Student, bool>> GetStudentInfo(string document)
         {
             return x => x.Document.Number == document;
         }
